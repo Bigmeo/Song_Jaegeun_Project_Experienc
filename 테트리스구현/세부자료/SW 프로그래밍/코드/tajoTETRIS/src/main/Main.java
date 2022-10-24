8ac53e79cec8603bbab323cac458bd65417df6af
Jaegeun Song <jaz5374@naver.com>
2022-10-24 22:55:12 +0900

    no message

---
+++ 테트리스구현/세부자료/SW 프로그래밍/코드/tajoTETRIS/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import view.MainMenuPopup;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new MainMenuPopup().setVisible(true);
+	}
+}
---
+++ 테트리스구현/세부자료/TEST/src/test01.java
@@ -0,0 +1,13 @@
+
+public class test01 {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("hi");
+		System.out.println("asdf");
+		System.out.println("qwer");
+		System.out.println("love");
+		System.out.println("zxcv");
+	}
+
+}
